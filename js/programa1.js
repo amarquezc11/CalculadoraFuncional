eecbe24c2ed6a51bdb35a3725c5b429745e5ac1a
Alberto Marquez <amarquezc09@gmail.com>
2023-12-18 15:34:37 -0500

    Empezamos a crear las funciones en JavaScript

--- js/programa1.js
+++ js/programa1.js
@@ -1,1 +1,30 @@
-alert("¡Hola, mundo!");
+let entradaActual = '';
+let operador = '';
+let respuesta = '';
+const pantalla = document.getElementsByClassName(pantalla);
+
+function limpiarPantalla(){
+    let entradaActual = '';
+    let operador = '';
+    let respuesta = '';
+    actualizarPantalla();
+}
+function borrarUltimodigito(){
+
+}
+function calcular(){
+
+}
+function asignarOperador(){
+
+}
+function agregarNumero(Number){
+entradaActual += Number;
+actualizarPantalla();
+}
+function agregarPunto(){
+
+}
+function actualizarPantalla(){
+pantalla.value = entradaActual;
+}
