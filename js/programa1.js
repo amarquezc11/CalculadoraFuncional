777f56cf48283f7b93360fb5ea683b7e6fbb69e6
Alberto Marquez <amarquezc09@gmail.com>
2023-12-22 15:55:35 -0500

    se terminaron las funciones, esta corriendo adecuadamente

--- js/programa1.js
+++ js/programa1.js
@@ -3,32 +3,36 @@ let operador = '';
 let respuesta = '';
 const pantalla = document.getElementById('pantalla');
 
-
-function actualizarPantalla(){
-pantalla.value = entradaActual;
-}
-
 function limpiarPantalla(){
-    let entradaActual = '';
-    let operador = '';
-    let respuesta = '';
+    entradaActual = '';
+    operador = '';
+    respuesta = '';
     actualizarPantalla();
 }
-
-function borrarUltimodigito(){
-
+function borrarUltimodigito() {
+    entradaActual = entradaActual.slice(0, -1);
+    actualizarPantalla();
 }
 function calcular(){
-
+    respuesta = eval(entradaActual);
+    entradaActual = respuesta;
+    operador = '';
+    actualizarPantalla();
 }
-function asignarOperador(){
- operador = op;
- entradaActual += ' ' + op + ' ';
+function asignarOperador(op){
+    operador = op;
+    entradaActual += '' + op + '';
 }
 function agregarNumero(number){
     entradaActual += number;
     actualizarPantalla();
 }
 function agregarPunto(){
-
+    if(!entradaActual.includes('.')){
+        entradaActual += '.';
+        actualizarPantalla();
+    }
+}
+function actualizarPantalla(){
+    pantalla.value = entradaActual;
 }
